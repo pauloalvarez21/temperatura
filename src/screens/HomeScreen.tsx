16d5df751292e3bd01cd93c31de5dda76f2ec835
paulo <pauloalvarez21@gmail.com>
2026-01-14 17:12:37 -0500

    feat: icon

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -205,7 +205,7 @@ const styles = StyleSheet.create({
     color: 'white',
     textAlign: 'center',
     marginBottom: 12,
-    fontFamily: 'CHOWFUN_',
+    fontFamily: 'CHOWFUN_.TTF',
   },
   heroSubtitle: {
     fontSize: 16,
